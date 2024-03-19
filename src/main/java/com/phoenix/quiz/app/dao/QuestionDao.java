a4481df54a755358072475a9529181af9f9216c0
pratikagupta <pratika.gupta008@gmail.com>
2024-03-19 13:34:50 +0530

    QuestionDao Error resolved

--- src/main/java/com/phoenix/quiz/app/dao/QuestionDao.java
+++ src/main/java/com/phoenix/quiz/app/dao/QuestionDao.java
@@ -11,6 +11,6 @@ import java.util.List;
 public interface QuestionDao extends JpaRepository<Question, Integer> {
     List<Question> findByCategory(String category);
 
-    @Query(value = "SELECT * FROM question a Where q.category=: category ORDER BY RANDOM() LIMIT :num", nativeQuery = true)
+    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
     List<Question> findRandomQuestionsByCategory(String category, int numQ);
 }
