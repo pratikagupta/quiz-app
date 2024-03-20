3e6f6b30252eafc98a168226aed0f7ad8825266d
pratikagupta <pratika.gupta008@gmail.com>
2024-03-20 16:38:30 +0530

    QuestionWrapper Code

--- src/main/java/com/phoenix/quiz/app/controller/QuizController.java
+++ src/main/java/com/phoenix/quiz/app/controller/QuizController.java
@@ -3,8 +3,8 @@ package com.phoenix.quiz.app.controller;
 
 import com.phoenix.quiz.app.model.QuestionWrapper;
 import com.phoenix.quiz.app.service.QuizService;
+import com.phoenix.quiz.app.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,12 +22,14 @@ public class QuizController {
         return quizService.createQuiz(category, numQ, title);
     }
 
-    @PostMapping("get/{id}")
+    @PostMapping ("get/{id}")
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
         return quizService.getQuizQuestions(id);
     }
 
-
-
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
+    }
 
 }
--- src/main/java/com/phoenix/quiz/app/dao/QuizDao.java
+++ src/main/java/com/phoenix/quiz/app/dao/QuizDao.java
@@ -2,6 +2,8 @@ package com.phoenix.quiz.app.dao;
 
 import com.phoenix.quiz.app.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuizDao extends JpaRepository<Quiz, Integer> {
 }
